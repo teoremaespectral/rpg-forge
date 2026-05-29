48d866fa64c4e66979e2f715783c383b9f08500d
Gabriel Luna <luna@matematica.ufrj.br>
2026-05-29 01:40:17 -0300

    feat: RollEngine class

---
+++ controller.py
@@ -0,0 +1,18 @@
+from models import Stat, NonNegativeStat, LimitedStat, Character, Player
+from random import randint
+
+class RollEngine:
+    '''Uma classe responsável por realizar rolagens de dados para um personagem com base em uma estatística específica.'''
+
+    def roll(self, character, stat_key, dice_number = 1, dice_sides = 20, modifier = 0):
+        stat = character.stats.get(stat_key)
+        if not stat:
+            return False
+                
+        rolls = [randint(1, dice_sides) for _ in range(dice_number)]
+        total = stat.value + sum(rolls) + modifier
+
+        return {
+            "rolls": rolls,
+            "total": total
+        }
